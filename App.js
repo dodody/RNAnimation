60b318d600f9341110a9c54037fe66d3214de01c
dody <dody@bigpearl.io>
2022-11-13 13:55:13 +0900

    리셋

--- App.js
+++ App.js
@@ -10,18 +10,16 @@ import AnimatedComposing from './src/chapter2/AnimatedComposing';
 import AnimatedOtherMethod from './src/chapter2/AnimatedOtherMethod';
 import AnimatedProperty from './src/chapter2/AnimatedProperty';
 
-import Snackbar from './src/chapter3/Snackbar';
-
 const App = () => {
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        // justifyContent: 'center',
-        // alignItems: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Snackbar />
+      <AnimatedProperty />
     </SafeAreaView>
   );
 };
