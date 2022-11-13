6880e5473741befbeab5f351e3fce3f5f0225d48
dody <dody@bigpearl.io>
2022-11-13 13:38:11 +0900

    초기 세팅

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+import AnimatedComponents from './src/chapter2/AnimatedComponents';
+import AnimatedValue from './src/chapter2/AnimatedValue';
+import AnimatedTiming from './src/chapter2/AnimatedTiming';
+import AnimatedSpring from './src/chapter2/AnimatedSpring';
+import AnimatedDecay from './src/chapter2/AnimatedDecay';
+import AnimatedComposing from './src/chapter2/AnimatedComposing';
+import AnimatedOtherMethod from './src/chapter2/AnimatedOtherMethod';
+import AnimatedProperty from './src/chapter2/AnimatedProperty';
+
+import Snackbar from './src/chapter3/Snackbar';
+
+const App = () => {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center',
+      }}
+    >
+      <Snackbar />
+    </SafeAreaView>
+  );
+};
+
+export default App;
